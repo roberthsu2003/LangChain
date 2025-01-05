2034a69d3f5fc57bb544be515a1cb95e8a1bce18
roberthsu2003 <roberthsu2003@gmail.com>
2025-01-05 07:51:38 +0000

    modify

--- 1_chat_models/1_chat_model_basic.py
+++ 1_chat_models/1_chat_model_basic.py
@@ -2,17 +2,17 @@
 # OpenAI Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/openai/
 
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 # Load environment variables from .env
 load_dotenv()
 
 # Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o")
+model = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
 
 # Invoke the model with a message
-result = model.invoke("What is 81 divided by 9?")
-print("Full result:")
+result = model.invoke("81除以9的答案是?")
+print("所有答案")
 print(result)
-print("Content only:")
+print("回答內容是")
 print(result.content)

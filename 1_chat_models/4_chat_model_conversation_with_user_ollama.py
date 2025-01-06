72d777fa69d7c66e1ba492ffbd71525d7994e28a
roberthsu2003 <roberthsu2003@gmail.com>
2025-01-06 08:44:31 +0000

    modify

---
+++ 1_chat_models/4_chat_model_conversation_with_user_ollama.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from langchain_ollama.llms import OllamaLLM
+# Load environment variables from .env
+load_dotenv()
+
+# Create a ChatOpenAI model
+model = OllamaLLM(model="llama3.2:3b")
+
+chat_history = []  # Use a list to store messages
+
+# Set an initial system message (optional)
+system_message = SystemMessage(content="You are a helpful AI assistant.")
+chat_history.append(system_message)  # Add system message to chat history
+
+# Chat loop
+while True:
+    query = input("You: ")
+    if query.lower() == "exit":
+        break
+    chat_history.append(HumanMessage(content=query))  # Add user message
+
+    # Get AI response using history
+    result = model.invoke(chat_history)
+    response = result
+    chat_history.append(AIMessage(content=response))  # Add AI message
+
+    print(f"AI: {response}")
+
+
+print("---- Message History ----")
+print(chat_history)
